25eb91f68f05c4892e6d7858a9ca44fb48fcd9fe
Andrii Volkov <qwerttyuiop050@ukr.net>
2024-05-18 19:42:41 +0300

    AddSeederDB

---
+++ Steam/Data/SeederDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Steam.Data.Entities.Identity;
+
+
+namespace Steam.Data
+{
+    public static class SeederDB
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var service = scope.ServiceProvider;
+
+                //Отримую посилання на наш контекст
+                var context = service.GetRequiredService<AppEFContext>();
+                context.Database.Migrate();
+
+                
+            }
+        }
+    }
+}
