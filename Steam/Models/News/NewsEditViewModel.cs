ba22dae46f82530941be492b32130c3efa94a057
Illya <illamatviuk@gmail.com>
2024-05-23 21:06:09 +0300

    Update NewsController and Models for News

--- Steam/Models/News/NewsEditViewModel.cs
+++ Steam/Models/News/NewsEditViewModel.cs
@@ -1,23 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Steam.Data.Entities
+namespace Steam.Models.News
 {
-    [Table("tblNews")]
-    public class NewsEntity : BaseEntity<int>
+    public class NewsEditViewModel
     {
-        [Required, StringLength(255)]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Title { get; set; }
-        [Required, StringLength(4000)]
+
+        [Required]
+        [StringLength(4000)]
         public string Description { get; set; }
+
         [Required]
         public DateTime DateOfRelease { get; set; }
-        [Required, StringLength(500)]
+
+        [Required]
+        [StringLength(500)]
         public string Image { get; set; }
-        [Required, StringLength(500)]
+
+        [Required]
+        [StringLength(500)]
         public string VideoURL { get; set; }
+
         [Required]
         public int GameId { get; set; }
-        public virtual GameEntity Game { get; set; }
     }
 }
